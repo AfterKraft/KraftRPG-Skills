77007e29c79410725df96848edc379c2be69c9ba
riking <rikingcoding@gmail.com>
2014-05-17 16:39:33 -0700

    Make changes

--- src/main/java/com/afterkraft/kraftrpg/bundled/SkillHarm.java
+++ src/main/java/com/afterkraft/kraftrpg/bundled/SkillHarm.java
@@ -1,18 +1,32 @@
+/*
+ * Copyright 2014 Gabriel Harris-Rouquette
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http:www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.afterkraft.kraftrpg.bundled;
 
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
 import com.afterkraft.kraftrpg.api.RPGPlugin;
-import com.afterkraft.kraftrpg.api.entity.Champion;
 import com.afterkraft.kraftrpg.api.entity.SkillCaster;
 import com.afterkraft.kraftrpg.api.skills.ActiveSkill;
-import com.afterkraft.kraftrpg.api.skills.SkillArgument;
+import com.afterkraft.kraftrpg.api.skills.Skill;
 import com.afterkraft.kraftrpg.api.skills.SkillCastResult;
 import com.afterkraft.kraftrpg.api.skills.arguments.EntitySkillArgument;
-import com.afterkraft.kraftrpg.api.util.ItemSkillRequirement;
+
 
 public class SkillHarm extends ActiveSkill {
-
     public SkillHarm(RPGPlugin plugin, String name) {
         super(plugin, name);
         setSkillArguments(new EntitySkillArgument<LivingEntity>(10.0, LivingEntity.class, null));
@@ -20,12 +34,7 @@ public class SkillHarm extends ActiveSkill {
 
     @Override
     public SkillCastResult useSkill(SkillCaster caster) {
-        this.<EntitySkillArgument<LivingEntity>> getArgument(0).getMatchedEntity().damage(5d, caster.getEntity());
-        return SkillCastResult.NORMAL;
-    }
-
-    @Override
-    public SkillCastResult canCast(SkillCaster caster, boolean forced) {
+        Skill.damageEntity(this.<EntitySkillArgument<LivingEntity>> getArgument(0).getMatchedEntity(), caster, 5.0, DamageCause.MAGIC);
         return SkillCastResult.NORMAL;
     }
 
@@ -33,29 +42,4 @@ public class SkillHarm extends ActiveSkill {
     public boolean grantsExperienceOnCast() {
         return false;
     }
-
-    @Override
-    public void initialize() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void shutdown() {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public boolean isInMessageRange(Champion broadcaster, Champion receiver) {
-        // XXX pull up
-        return false;
-    }
-
-    @Override
-    public SkillCastResult canUse(SkillCaster caster) {
-        // TODO
-        return SkillCastResult.NORMAL;
-    }
-
 }
