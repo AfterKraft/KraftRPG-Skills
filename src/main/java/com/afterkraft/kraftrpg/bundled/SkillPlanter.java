8266e8448445d259101fb9150506fc736bda4eee
Gabriel Harris-Rouquette <gabizou@me.com>
2014-05-25 21:53:37 -0700

    Add converted skill by Falker.

---
+++ src/main/java/com/afterkraft/kraftrpg/bundled/SkillPlanter.java
@@ -0,0 +1,111 @@
+package com.afterkraft.kraftrpg.bundled;
+
+import java.util.Collection;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.configuration.Configuration;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+
+import com.afterkraft.kraftrpg.api.RPGPlugin;
+import com.afterkraft.kraftrpg.api.entity.SkillCaster;
+import com.afterkraft.kraftrpg.api.skills.ActiveSkill;
+import com.afterkraft.kraftrpg.api.skills.SkillCastResult;
+import com.afterkraft.kraftrpg.api.skills.SkillSetting;
+
+
+public class SkillPlanter extends ActiveSkill implements Listener {
+
+    public SkillPlanter(RPGPlugin plugin) {
+        super(plugin, "Planter");
+        setDescription("Plant the Ground");
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
+
+    public Configuration getDefaultConfig() {
+        return super.getDefaultConfig();
+    }
+
+    @Override
+    public Collection<SkillSetting> getUsedConfigNodes() {
+        return null;
+    }
+
+    public SkillCastResult useSkill(SkillCaster caster) {
+        int radius = 2;
+        final World world = caster.getWorld();
+        final int x = caster.getLocation().getBlockX();
+        final int y = caster.getLocation().getBlockY();
+        final int z = caster.getLocation().getBlockZ();
+        final int minX = x - radius;
+        final int minZ = z - radius;
+        final int maxX = x + radius;
+        final int maxZ = z + radius;
+        int carrotsPlanted = 0;
+        int potatoesPlanted = 0;
+        int count = 0;
+        int count2 = 0;
+
+        if (caster.isOnGround()) {
+            for (int counterX = minX; counterX <= maxX; counterX++) {
+                for (int counterZ = minZ; counterZ <= maxZ; counterZ++) {
+                    final Block blockName = world.getBlockAt(counterX, y, counterZ);
+                    final Block lowerblock = world.getBlockAt(counterX, y - 1, counterZ);
+                    final Location lowerLoc = lowerblock.getLocation();
+                    final Location locBlock = blockName.getLocation();
+                    if (caster.getItemInHand().getType().equals(Material.SEEDS)) {
+                        if (caster.getInventory().contains(Material.SEEDS)) {
+                            if (locBlock.getBlock().getType().equals(Material.CROPS) && lowerLoc.getBlock().getType().equals(Material.SOIL)) {
+                                locBlock.getBlock().setType(Material.CROPS);
+                                caster.getInventory().removeItem(new ItemStack(Material.SEEDS, 1));
+                                caster.updateInventory();
+                            } else {
+                                count2++;
+                            }
+                        } else {
+                            count++;
+                        }
+                    }
+                    if (caster.getItemInHand().getType().equals(Material.CARROT_ITEM)) {
+                        if (locBlock.getBlock().getType().equals(Material.CARROT) && lowerLoc.getBlock().getType().equals(Material.SOIL)) {
+                            locBlock.getBlock().setType(Material.CARROT);
+                            carrotsPlanted += 1;
+                        } else {
+                            count2++;
+                        }
+                    }
+                    if (caster.getItemInHand().getType().equals(Material.POTATO_ITEM)) {
+                        if ((locBlock.getBlock().getType().equals(Material.POTATO)) && (lowerLoc.getBlock().getType().equals(Material.SOIL))) {
+                            locBlock.getBlock().setType(Material.POTATO);
+                            potatoesPlanted += 1;
+                        }
+                    } else {
+                        count2++;
+                    }
+                }
+            }
+        } else {
+            caster.sendMessage("You must be on the ground to Plant.");
+            return SkillCastResult.FAIL;
+        }
+        if (count > 0)
+            caster.sendMessage("You have run out of seeds. " + count + " seeds not planted.");
+        if (count2 > 0)
+            caster.sendMessage(count2 + " crops have not been planted due to crops already being there.");
+        caster.getInventory().removeItem(new ItemStack(Material.CARROT_ITEM, carrotsPlanted));
+        caster.getInventory().removeItem(new ItemStack(Material.POTATO_ITEM, potatoesPlanted));
+        caster.updateInventory();
+        //        broadcastExecuteText(caster); // TODO Implement skill message broadcasting
+        return SkillCastResult.NORMAL;
+
+    }
+
+    @Override
+    public boolean grantsExperienceOnCast() {
+        return false;
+    }
+}
