4b4e6562bd4b72468ab2a07faf63b026d05352fb
Gabriel Harris-Rouquette <gabizou@me.com>
2014-09-07 10:44:37 -0700

    License Change.

--- src/main/java/com/afterkraft/kraftrpg/bundled/skills/SkillMegabolt.java
+++ src/main/java/com/afterkraft/kraftrpg/bundled/skills/SkillMegabolt.java
@@ -1,6 +1,33 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Gabriel Harris-Rouquette
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.afterkraft.kraftrpg.bundled.skills;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
 
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -11,7 +38,9 @@ import org.bukkit.inventory.ItemStack;
 
 import com.afterkraft.kraftrpg.api.RPGPlugin;
 import com.afterkraft.kraftrpg.api.entity.IEntity;
+import com.afterkraft.kraftrpg.api.entity.Insentient;
 import com.afterkraft.kraftrpg.api.entity.SkillCaster;
+import com.afterkraft.kraftrpg.api.events.entity.damage.InsentientDamageEvent.DamageType;
 import com.afterkraft.kraftrpg.api.skills.Skill;
 import com.afterkraft.kraftrpg.api.skills.SkillCastResult;
 import com.afterkraft.kraftrpg.api.skills.SkillSetting;
@@ -42,14 +71,15 @@ public class SkillMegabolt extends TargetedSkill<LivingEntity> {
         target.getWorld().strikeLightning(target.getLocation());
         target.getWorld().playSound(target.getLocation(), Sound.AMBIENCE_THUNDER, volume, 1.0F);
 
-        Skill.damageEntity(entity, caster, damage, DamageCause.MAGIC);
+        Skill.damageEntity((Insentient) target, caster, this, ImmutableMap.of(DamageType.MAGICAL, damage), DamageCause.MAGIC);
 
         List<Entity> entities = target.getNearbyEntities(radius, radius, radius);
-        for (Entity nearbyEntity: entities) {
+        for (Entity nearbyEntity : entities) {
             if (!(nearbyEntity instanceof LivingEntity)) {
                 continue;
             }
             LivingEntity nearbyLivingEntity = (LivingEntity) nearbyEntity;
+            Insentient insentient = (Insentient) this.plugin.getEntityManager().getEntity(nearbyLivingEntity);
             if (!damageCheck(caster, nearbyLivingEntity)) {
                 continue;
             }
@@ -57,7 +87,7 @@ public class SkillMegabolt extends TargetedSkill<LivingEntity> {
             nearbyLivingEntity.getWorld().strikeLightning(nearbyLivingEntity.getLocation());
             nearbyLivingEntity.getWorld().playSound(nearbyLivingEntity.getLocation(), Sound.AMBIENCE_THUNDER, volume, 1.0F);
             addSkillTarget(nearbyEntity, caster);
-            Skill.damageEntity(nearbyLivingEntity, caster, damage, DamageCause.MAGIC);
+            Skill.damageEntity(insentient, caster, this, ImmutableMap.of(DamageType.MAGICAL, damage), DamageCause.MAGIC);
         }
 
         return null;
